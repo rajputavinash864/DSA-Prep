07e3c43a436dbcdc9bf8f598f1e50209ea5c8d6a
Avinash Kumar <rajputavinash864@gmail.com>
2025-07-05 03:54:07 +0530

    Done first question

--- Day1.cpp
+++ Day1.cpp
@@ -1,10 +1,28 @@
+// Qno -1  Numeric Hollow half pyramid 
+
+
 #include<iostream>
 using namespace std;
 
 int main(){
 
-    cout << "Namaste Duniya" << endl;
+    int n;
+    cin >> n;
+
+    for(int i = 0;i < n;i++){
+
+        for (int j = 0;j < i + 1;j++){
+
+            if(j == 0 || j == i || i == n-1){
+                cout << j+1;
+            }
+            else {
+                cout << " ";
+            }
+        }
+        cout << endl;
+    }
 
 
     return 0;
-}
+} 
